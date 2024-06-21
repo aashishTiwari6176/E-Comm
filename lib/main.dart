b776e6ff648f1d6c1193215d31e4d85200eca497
AASHISH TIWARI <138970451+aashishTiwari6176@users.noreply.github.com>
2024-06-22 01:25:47 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:e_commerce_app/app.dart';
+import 'package:e_commerce_app/features/authentication/screens/onbording/widgets/onbording_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
